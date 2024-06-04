Initial attempt at inpainting, box type and random type masking from DPS paper, gradient term zeroing out after iter1 - needs fixing. 6.3.2024

dps.py
@@ -90,7 +90,7 @@ class DPS:
         return torch.matmul(torch.matmul(res.T, lam), res)
 
     def learned_range_model_log_var(self, i, learned_sigma):
-        min_log = self.posterior_log_variance_clipped[i]
+        min_log = self.ddpm_log_posterior_var_clipped[i]
         max_log = torch.log(self.betas)[i]
         # The model_var_values is [-1, 1] for [min_var, max_var].
         frac = (learned_sigma + 1) / 2
@@ -144,16 +144,16 @@ class DPS:
             # Likelihood calculations for DPS
             forward_x0_hat = A_operation(x0_hat_clipped)
             res = y - forward_x0_hat
-            residual = res.detach().requires_grad_(True)
             if self.model_noise_type.lower() == 'gaussian':
-                loss = self.gaussian_loss(residual)
+                loss = self.gaussian_loss(res)
             elif self.model_noise_type.lower() == 'poisson':
-                loss = self.poisson_loss(residual, y)
+                loss = self.poisson_loss(res, y)
             else:
                 # Currently only supporting Gaussian and Poisson from DPS paper formulations
                 raise NotImplementedError(f"Unknown noise type: {self.model_noise_type}")
             grad_term = torch.autograd.grad(loss, x, create_graph=True)[0]
-            true_step = self.step_size_factor/residual # see footnotes #5 as mentioned in init function
+            print(f"Gradient term at step {i}: {grad_term}")
+            true_step = self.step_size_factor/res # see footnotes #5 as mentioned in init function
             correction_term = -true_step*grad_term
             x = ddpm_posterior + correction_term
         return x

inpainting.py
@@ -5,6 +5,24 @@ import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 
+def box_type(image, box_size=128, margin=16):
+    """Box-type inpainting, as specified by DPS paper [1] and Chung et all 2022a [2], this means that:
+     - we mask out a 128×128 box region from the image
+     - the location of the box is sampled uniformly within 16 pixel margin of each side
+    """
+    h, w = image.shape[-2:]
+    
+    start_h = np.random.randint(margin, h - margin - box_size)
+    start_w = np.random.randint(margin, w - margin - box_size)
+    
+    mask = torch.ones_like(image)
+    mask[:, :, start_h:start_h+box_size, start_w:start_w+box_size] = 0
+    return image * mask
+
+def random_type(image, mask_amount = 0.92):
+    mask = torch.rand(image.shape[0], 1, image.shape[2], image.shape[3], device=image.device) > mask_amount
+    return image * mask
+
 if __name__ == "__main__":
     dps = DPS()
     idx = 0
@@ -13,12 +31,21 @@ if __name__ == "__main__":
     y = next(data_iter)
 
     # # Display the image
-    # plt.imshow(y)
+    # plt.imshow(np.transpose(y[0], (1, 2, 0)))
     # plt.axis('off')  # Turn off axis
     # plt.show()
-    
-    x = dps.sample_conditional_posterior(y)
-    # plt.imshow(x)
-    # plt.axis('off')  # Turn off axis
-    # plt.show()
+
+    box = box_type(y)    
+    plt.imshow(np.transpose(box[0], (1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+
+    # bruh = random_type(y)
+    # plt.imshow(np.transpose(bruh[0], (1, 2, 0)))
+    # plt.axis('off')
+
+    x = dps.sample_conditional_posterior(box, box_type)
+    plt.imshow(x)
+    plt.axis('off')  # Turn off axis
+    plt.show()
 